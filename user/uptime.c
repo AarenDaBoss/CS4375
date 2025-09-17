e73bb7826a0fcb4461cf0604e3376912de7fb75d
vvrennn <floresaaren@gmail.com>
2025-09-17 15:05:11 -0600

    Added my uptime function

--- user/uptime.c
+++ user/uptime.c
@@ -1,8 +1,9 @@
-#include "stdio.h"
-#include "user.h"
+//#include "stdio.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
 
 int main(int argc,char *argv[]){
-  int numticks = uptime();
-  printf("%d",numticks);
+  fprintf(1,"Up %d clock ticks\n",uptime());
   exit(0);
 }
