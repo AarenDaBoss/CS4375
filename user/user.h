34ebd455f64361253c367fe53a31462382bd296c
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2025-10-03 13:05:29 -0600

    HW3PS

--- user/user.h
+++ user/user.h
@@ -1,5 +1,6 @@
 struct stat;
 struct rtcdate;
+struct pstat; //hw3 getprocs
 
 // system calls
 int fork(void);
@@ -23,6 +24,7 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+int getprocs(struct pstat*); //hw3 getprocs
 
 // ulib.c
 int stat(const char*, struct stat*);
